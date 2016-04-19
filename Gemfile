ee88622e25cd61a61fd707ffba405059d65e4246
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-04-19 07:21:54 -0700

    Add This Week in Ruma.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
-# If you do not have OpenSSL installed, change
-# the following line to use 'http://'
 source 'https://rubygems.org'
 
 # For faster file watcher updates on Windows:
@@ -8,7 +6,6 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 # Windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
-# Middleman Gems
-gem 'middleman', '>= 4.0.0'
+gem 'middleman'
+gem 'middleman-blog'
 gem 'middleman-livereload'
-gem 'middleman-autoprefixer'
--- config.rb
+++ config.rb
@@ -1,3 +1,19 @@
+activate :blog do |blog|
+  blog.default_extension = ".md"
+  blog.generate_day_pages = false
+  blog.generate_month_pages = false
+  blog.generate_tag_pages = false
+  blog.generate_year_pages = false
+  blog.layout = "news"
+  blog.paginate = true
+  blog.per_page = 3
+  blog.permalink = "this-week-in-ruma-{year}-{month}-{day}.html"
+  blog.prefix = "news"
+  blog.sources = "{year}-{month}-{day}.html"
+end
+
+activate :directory_indexes
+
 configure :development do
   activate :livereload
 end
