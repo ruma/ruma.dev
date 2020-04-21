0c39ec1d0f77b186b1416f0f87b7524c60367c39
Jonas Platte <jplatte+git@posteo.de>
2020-04-21 16:05:51 +0200

    Delete old deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-bundle exec middleman build
-pushd build
-aws s3 sync --delete . s3://www.ruma.io
-aws cloudfront create-invalidation --distribution-id E2OENHN48B3NC4 --paths '/*'
